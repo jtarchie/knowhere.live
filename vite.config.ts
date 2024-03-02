ce7acbdd8ab0a5c8e199e28f32978c1c336c739a
JT Archie <jtarchie@gmail.com>
2024-03-02 13:32:58 -0700

    include source map

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import preact from '@preact/preset-vite'
 export default defineConfig({
   plugins: [preact()],
   build: {
+    sourcemap: true,
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
