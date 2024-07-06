6ff9d537e07d48dd172b52c9351ee6d6ea20e542
JT Archie <jtarchie@gmail.com>
2024-07-05 18:45:36 -0600

    build docs

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Knowhere API Docs",
+  description: "API documentation for the Knowhere API.",
+  outDir: "../dist/docs",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
