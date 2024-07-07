9860417891ef9069a0cc5fe146f3565a414e0b1a
JT Archie <jtarchie@gmail.com>
2024-07-06 21:03:39 -0600

    add back frontpage

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { render } from "preact";
 import Router from "preact-router";
 import { Render } from "./pages/render";
+import { Home } from "./pages/home";
 
 function App() {
   return (
     <Router>
-      <Render path="/" />
+      <Render path="/beta" />
+      <Home path="/" />
     </Router>
   );
 }
