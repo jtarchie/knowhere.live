93d22d649e8caac213c9305d8c9f85d67f702b71
JT Archie <jtarchie@gmail.com>
2024-07-08 11:51:33 -0600

    delay loading of modules

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import { render } from "preact";
 import Router from "preact-router";
-import { Render } from "./pages/render";
+import AsyncRoute from "preact-async-route";
 import { Home } from "./pages/home";
 
 function App() {
   return (
     <Router>
-      <Render path="/beta" />
+      <AsyncRoute
+        path="/beta"
+        getComponent={() =>
+          import("./pages/render").then((module) => module.Render)}
+      />
       <Home path="/" />
     </Router>
   );
