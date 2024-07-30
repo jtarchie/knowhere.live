d0d065ef49fd84d1912eeb15e776560bd314bc2a
JT Archie <jtarchie@gmail.com>
2024-07-30 10:06:52 -0600

    fix editor view

--- src/components/bottom-nav.tsx
+++ src/components/bottom-nav.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "preact/hooks";
 import { FormSchema } from "../form/types";
 
 function BottomNav() {
+  const params = new URLSearchParams(window.location.search);
+
   const [filter, setFilter] = useState<FormSchema>([]);
   const currentPath = window.location.pathname;
   const active = (path: string) => currentPath == path ? "active" : "";
@@ -34,27 +36,10 @@ function BottomNav() {
         </svg>
         <span class="btm-nav-label">Map</span>
       </button>
-      <button class={active("/beta/editor")} onClick={redirect("/beta/editor")}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="size-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
-          />
-        </svg>
-        <span class="btm-nav-label">Editor</span>
-      </button>
-      {filter && filter.length > 0 && (
+      {params.get("editor") && (
         <button
-          class={active("/beta/filter")}
-          onClick={redirect("/beta/filter")}
+          class={active("/beta/editor")}
+          onClick={redirect("/beta/editor")}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -67,13 +52,33 @@ function BottomNav() {
             <path
               stroke-linecap="round"
               stroke-linejoin="round"
-              d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75"
+              d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
             />
           </svg>
-
-          <span class="btm-nav-label">Filter</span>
+          <span class="btm-nav-label">Editor</span>
         </button>
       )}
+      <button
+        class={active("/beta/filter")}
+        onClick={redirect("/beta/filter")}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75"
+          />
+        </svg>
+
+        <span class="btm-nav-label">Filter</span>
+      </button>
     </div>
   );
 }
