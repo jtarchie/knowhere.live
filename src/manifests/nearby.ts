3695ea1ad23177da7deb1b4e9609fb5a661e91ef
JT Archie <jtarchie@gmail.com>
2024-08-09 13:36:43 -0600

    update to latest API

--- src/manifests/nearby.ts
+++ src/manifests/nearby.ts
@@ -76,7 +76,7 @@ const source = `
   Object.keys(params).forEach((key, index) => {
     if (filters[key] && params[key] == "1") {
       const asQueries = filters[key].map((query) =>
-        \`nwr\${query}(prefix=\${prefix})(bb=\${boundary.min[0]},\${boundary.min[1]},\${boundary.max[0]},\${boundary.max[1]})\`
+        \`nwr\${query}(prefix=\${prefix})(bb=\${boundary.min()[0]},\${boundary.min()[1]},\${boundary.max()[0]},\${boundary.max()[1]})\`
       );
       const results = query.union(...asQueries);
 
