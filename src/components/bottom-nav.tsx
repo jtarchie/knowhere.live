a32db4351fbd93a8602f798997b5ede368bdf7b0
JT Archie <jtarchie@gmail.com>
2024-08-09 21:17:42 -0600

    remove tooltip on bottom nav

--- src/components/bottom-nav.tsx
+++ src/components/bottom-nav.tsx
@@ -1,12 +1,8 @@
 import { route } from "preact-router";
 import qs from "qs";
 import { Manager } from "../render/manager";
-import { useRef } from "preact/hooks";
-import { RefObject } from "preact";
 
 function BottomNav({ manifestName }: { manifestName?: string }) {
-  const tooltipRef = useRef<HTMLDivElement>();
-
   const params = qs.parse(window.location.search.slice(1));
 
   const currentPath = window.location.pathname;
@@ -14,14 +10,32 @@ function BottomNav({ manifestName }: { manifestName?: string }) {
   const redirect = (path: string) => () => route(path);
 
   return (
-    <div class="btm-nav bg-base-100 flex">
-      <div
-        class="tooltip tooltip-primary"
-        data-tip="See results on map"
+    <div class="btm-nav bg-base-100 relative">
+      <button
+        class={active(`/beta/${manifestName}/map`) +
+          " text-primary"}
+        onClick={redirect(`/beta/${manifestName}/map`)}
       >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M9 6.75V15m6-6v8.25m.503 3.498 4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 0 0-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0Z"
+          />
+        </svg>
+        <span class="btm-nav-label">Map</span>
+      </button>
+      {params.editor && (
         <button
-          class={active(`/beta/${manifestName}/map`) + " text-primary"}
-          onClick={redirect(`/beta/${manifestName}/map`)}
+          class={active("/beta/editor")}
+          onClick={redirect("/beta/editor")}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -34,119 +48,71 @@ function BottomNav({ manifestName }: { manifestName?: string }) {
             <path
               stroke-linecap="round"
               stroke-linejoin="round"
-              d="M9 6.75V15m6-6v8.25m.503 3.498 4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 0 0-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0Z"
+              d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
             />
           </svg>
-          <span class="btm-nav-label">Map</span>
+          <span class="btm-nav-label">Editor</span>
         </button>
-        {params.editor && (
-          <button
-            class={active("/beta/editor")}
-            onClick={redirect("/beta/editor")}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="size-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
-              />
-            </svg>
-            <span class="btm-nav-label">Editor</span>
-          </button>
-        )}
-      </div>
-      <div
-        class="tooltip tooltip-primary"
-        data-tip="Filter results to display on the map"
+      )}
+      <button
+        class={active(`/beta/${manifestName}/filter`) + " text-primary"}
+        onClick={redirect(`/beta/${manifestName}/filter`)}
       >
-        <button
-          class={active(`/beta/${manifestName}/filter`) + " text-primary"}
-          onClick={redirect(`/beta/${manifestName}/filter`)}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-6"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="size-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75"
-            />
-          </svg>
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75"
+          />
+        </svg>
 
-          <span class="btm-nav-label">Filter</span>
-        </button>
-      </div>
-      <div
-        class="tooltip tooltip-primary"
-        ref={tooltipRef as RefObject<HTMLDivElement>}
-        data-tip="Share the URL"
+        <span class="btm-nav-label">Filter</span>
+      </button>
+      <button
+        onClick={async () => {
+          const manager = new Manager();
+          const { values } = manager.load(manifestName);
+          const url =
+            `${import.meta.env.VITE_WEB_ADDRESS}/beta/${manifestName}/map?${
+              qs.stringify({ values: values })
+            }`;
+
+          try {
+            await navigator.share({
+              title: "Knowhere to Live",
+              url: url,
+            });
+          } catch (err) {
+            console.log("Native share not supported", err);
+            await navigator.clipboard.writeText(url);
+          }
+        }}
+        class="text-primary"
       >
-        <button
-          onClick={async () => {
-            const manager = new Manager();
-            const { values } = manager.load(manifestName);
-            const url =
-              `${import.meta.env.VITE_WEB_ADDRESS}/beta/${manifestName}/map?${
-                qs.stringify({ values: values })
-              }`;
-
-            let tooltipMessage = tooltipRef.current?.getAttribute("data-tip");
-
-            try {
-              await navigator.share({
-                title: "Knowhere to Live",
-                url: url,
-              });
-              tooltipMessage = "Shared successfully!";
-
-              setTimeout(() => {
-                tooltipRef.current?.classList.remove("tooltip-open");
-              }, 2000);
-            } catch (err) {
-              console.log("Native share not supported", err);
-              await navigator.clipboard.writeText(url);
-              tooltipMessage = "Copied to clipboard!";
-            }
-
-            tooltipRef.current?.setAttribute("data-tip", tooltipMessage);
-            tooltipRef.current?.classList.add("tooltip-open");
-            setTimeout(() => {
-              tooltipRef.current?.classList.remove("tooltip-open");
-              tooltipRef.current?.setAttribute("data-tip", "Share the URL");
-            }, 2000);
-          }}
-          class="text-primary"
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-6"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="size-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z"
-            />
-          </svg>
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z"
+          />
+        </svg>
 
-          <span class="btm-nav-label">Share</span>
-        </button>
-      </div>
+        <span class="btm-nav-label">Share</span>
+      </button>
     </div>
   );
 }
