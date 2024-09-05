42ce6ea85407385c91f4a73cc4f8752286d61177
JT Archie <jtarchie@gmail.com>
2024-09-05 13:58:05 -0600

    try different walk scores

--- src/manifests/nearby.ts
+++ src/manifests/nearby.ts
@@ -72,6 +72,46 @@ const filters: { [key: string]: { queries: string[]; markerSymbol: string } } =
 
 /* @__PURE__ */
 const source = `
+  const walkScore = (distance, limit) => {
+    // return 100 * Math.max(0, 1 - (distance / limit));
+    return 100 * Math.exp(-0.5 * (distance / limit));
+  }
+
+  const median = (values) => {
+    values.sort((a, b) => a - b);
+    const mid = Math.floor(values.length / 2);
+
+    if (values.length % 2 !== 0) {
+        return values[mid];
+    }
+    
+    return (values[mid - 1] + values[mid]) / 2;
+  }
+
+  const harmonicMean = (values) => {
+    // Calculate the sum of reciprocals (1/value) for each value in the array
+    const reciprocalSum = values.reduce((sum, value) => sum + (1 / value), 0);
+
+    // The harmonic mean is the number of values divided by the reciprocal sum
+    return values.length / reciprocalSum;
+  }
+
+  const average = (values) => {
+    const sum = values.reduce((sum, value) => sum + value, 0)
+
+    return sum / values.length
+  }
+
+  const overallScore = (distances, limit) => {
+    const values = distances.filter(distance => distance <= limit);
+    if (values.length === 0) return 0;
+
+    assert.stab(JSON.stringify(values));
+    const scores = values.map( distance => walkScore(distance, limit) );
+    assert.stab(JSON.stringify(scores));
+    return median(scores);
+  }
+
   const filters = ${JSON.stringify(filters)};
   const [fullAddress, lat, lon] = params.address.split("|");
   const center = geo.asPoint(lat || 39.7401684, lon || -104.9894902);
@@ -79,20 +119,18 @@ const source = `
   if (!found) {
     throw new Error("could not parse address for state")
   }
-  let features = [
-    center.asFeature({
-      "marker-color": colors.pick(0),
-      "title": fullAddress,
-    }),
-  ];
+
   const area = parsedAddress.state.toLowerCase();
-  
-  const boundary = center.asBound().extend(params.radius); //meters
+  const boundary = center.asBound().extend(params.radius);
+  let features = [];
+
   features.push(boundary.asFeature({
     "fill": colors.pick(0),
     "fill-opacity": 0,
     "stroke": colors.pick(0),
-  }))
+  }));
+
+  const distances = [];
 
   Object.keys(params).forEach((key, index) => {
     if (filters[key] && params[key] == "1") {
@@ -102,6 +140,8 @@ const source = `
       const results = query.union(...asQueries);
 
       features = features.concat(results.map((result) => {
+        distances.push(geo.distance(center.asBound(), result.bound()) / 1000);
+
         return result.asFeature({
           "marker-color": colors.pick(index),
           "title": result.tags.name,
@@ -111,6 +151,23 @@ const source = `
       }));
     }
   });
+
+  const scores = [
+    ["walk", 1.6],
+    ["bike", 8.0],
+    ["drive", 15] 
+  ];
+
+  const score = scores.map(([type, limit]) => {
+    return type + ": " + (overallScore(distances, limit) | 0) + "%";
+  }).join("\\n")
+
+  features.push(
+    center.asFeature({
+      "marker-color": colors.pick(0),
+      "title": fullAddress + "\\n" + score,
+    })
+  );
 
   const payload = {
     type: "FeatureCollection",
