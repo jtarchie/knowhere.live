83c39ed9e97a948a51c5b9b66d5c9f51bce58c8e
JT Archie <jtarchie@gmail.com>
2024-11-16 06:35:38 -0700

    add first test

---
+++ test/usage.test.ts
@@ -0,0 +1,45 @@
+import { afterAll, beforeAll, describe, test } from "vitest";
+import { chromium } from "playwright";
+import type { Browser, Page } from "playwright";
+import { expect } from "@playwright/test";
+import { ChildProcess, spawn } from "child_process";
+import { setTimeout } from "timers/promises";
+
+describe("create and use a retro", () => {
+  let serverProcess: ChildProcess;
+  let browser: Browser;
+  let leader: Page;
+
+  beforeAll(async () => {
+    serverProcess = spawn("yarn", ["run", "wrangler", "pages", "dev"], {
+      stdio: ["inherit", "pipe", "pipe"],
+      shell: true,
+    });
+
+    serverProcess.stdout?.on("data", (data) => console.log(`${data}`));
+    serverProcess.stderr?.on("data", (data) => console.error(`${data}`));
+
+    await setTimeout(2000);
+
+    browser = await chromium.launch({ headless: true });
+    leader = await browser.newPage();
+  });
+
+  afterAll(async () => {
+    await browser.close();
+    serverProcess.kill();
+  });
+
+  test("search for a costco", async () => {
+    await leader.goto("http://localhost:8788/beta/demo/map");
+    await leader.getByRole("button", { name: "Filter" }).click();
+    expect(leader.url()).toContain("/beta/demo/filter");
+
+    await leader.screenshot({ path: "screenshot.png" });
+    await leader.getByLabel("Name").fill("Starbucks");
+    await leader.getByLabel("Area").selectOption("new_york");
+    await leader.getByRole("button", { name: "Apply" }).click();
+
+    expect(leader.url()).toContain("/beta/demo/map");
+  }, 10_000);
+});
